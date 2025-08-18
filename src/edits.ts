485c3efcd3db0e6490e0e664ca40970e0beeb438
wildbook <book.wille@gmail.com>
2025-08-18 18:23:41 +0200

    darken the entire map for better contrast

--- src/edits.ts
+++ src/edits.ts
@@ -1,12 +1,64 @@
-import { e, m, TForm } from "./utils/bin.js";
+import { xxh64 } from "@node-rs/xxhash";
+import { dbg, e, m, TForm } from "./utils/bin.js";
 
 import { t } from "./utils/bin_types.js";
+import logger from "node-color-log";
 
 type Edits = {
   [wadPath: string]: { [bin: string]: TForm };
 };
 
-export const edits: Edits = {
+function path(str: string): string {
+  return xxh64(str, BigInt(0)).toString(16).padStart(16, "0") + ".json";
+}
+
+export const darkenMap: Edits = {
+  // Summoner's Rift - Map 11
+  "Maps/Shipping/Map11.wad.client": {
+    // Map Skin - Spirit Blossom
+    [path("data/maps/mapgeometry/map11/bloom.materials.bin")]: [
+      e.iterate((x) => {
+        let val = x.value;
+        if (val.name !== "StaticMaterialDef") {
+          return;
+        }
+
+        let name: string = val.items.find((x) => x.key === "Name")?.value;
+        let deny = ![
+          "Maps/KitPieces/SRS/Bloom/Materials/Default/Bloom_Ground", // Global ground
+
+          "Maps/KitPieces/SRS/Bloom/Materials/Default/Bloom_Chaos_Spawn_A_MAT", // Chaos spawn
+          "Maps/KitPieces/SRS/Bloom/Materials/Default/Bloom_Order_Spawn_A_MAT", // Order spawn
+
+          "Maps/KitPieces/SRS/Bloom/Materials/Default/Bloom_Chaos_Base_A_MAT", // Upper half
+          "Maps/KitPieces/SRS/Bloom/Materials/Default/Bloom_Chaos_Base_B_MAT", // Lower half
+
+          "Maps/KitPieces/SRS/Bloom/Materials/Default/Bloom_Order_Base_A_MAT", // Upper half
+          "Maps/KitPieces/SRS/Bloom/Materials/Default/Bloom_Order_Base_B_MAT", // Lower half
+        ].find((x) => name.startsWith(x));
+
+        if (deny) {
+          return;
+        }
+
+        let params = val.items.find((x) => x.key === "ParamValues");
+        for (const param of params.value.items) {
+          let key = param.items.find((x) => x.key === "Name");
+          let val = param.items.find((x) => x.key === "Value");
+
+          if (!["TintColor", "BaseTex_TintColor"].includes(key.value)) {
+            continue;
+          }
+
+          logger.debug(`Modified ${name} (${key.value})`);
+          val.value = [0.3, 0.3, 0.3, 0];
+        }
+      }),
+    ],
+  },
+};
+
+export const xerath: Edits = {
   "Champions/Xerath.wad.client": {
     "a120033c1ad32987.json": [
       "Characters/Xerath/Skins/Skin5/Particles/Xerath_Skin05_Q_aoe_reticle_red",
@@ -22,6 +74,11 @@ export const edits: Edits = {
       ]),
     ],
   },
+};
+
+export const edits: Edits = {
+  ...darkenMap,
+  ...xerath,
 };
 
 export default edits;
